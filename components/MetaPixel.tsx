9629d2817b8e520eebce06b8f2ec02ec94a1b6e4
rutasconfiables-cl <rutasconfiables.cl@gmail.com>
2026-08-16 15:01:30 -0400

    Make Meta Pixel page views reliable

--- components/MetaPixel.tsx
+++ components/MetaPixel.tsx
@@ -2,7 +2,7 @@
 
 import Script from "next/script";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 
 declare global {
   interface Window { fbq?: (...args: unknown[]) => void }
@@ -12,18 +12,23 @@ const pixelId = "1535704924960344";
 
 export default function MetaPixel() {
   const pathname = usePathname();
-  const [ready, setReady] = useState(false);
+  const initialPageView = useRef(true);
 
   useEffect(() => {
-    if (ready) window.fbq?.("track", "PageView");
-  }, [ready, pathname]);
+    if (initialPageView.current) {
+      initialPageView.current = false;
+      return;
+    }
+    window.fbq?.("track", "PageView");
+  }, [pathname]);
 
-  return <Script id="meta-pixel" strategy="afterInteractive" onReady={() => setReady(true)} dangerouslySetInnerHTML={{__html:`
+  return <Script id="meta-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{__html:`
     !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
     n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
     n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
     t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
     (window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
     fbq('init','${pixelId}');
+    fbq('track','PageView');
   `}}/>;
 }
